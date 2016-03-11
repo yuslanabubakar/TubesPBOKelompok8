a881d57dad0429a12d4cb3f863ea86d37e629e90
yuslanabubakar <yuslan.black@gmail.com>
2016-03-12 03:41:16 +0700

    Merge origin/master

---
+++ RegistrasiMK/src/registrasimk/Kelas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registrasimk;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PRAKTIKUM
+ */
+public class Kelas {
+    private Matakuliah matakuliah;
+    private Dosen dosen;
+
+
+    public Matakuliah getMatakuliah() {
+        return matakuliah;
+    }
+
+    public void setMatakuliah(Matakuliah matakuliah) {
+        this.matakuliah = matakuliah;
+    }
+
+    public Dosen getDosen() {
+        return dosen;
+    }
+
+    public void setDosen(Dosen dosen) {
+        this.dosen = dosen;
+    }
+}
